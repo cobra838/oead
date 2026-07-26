b5c26e6004f297462d580773b1278121929d833c
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-07-26 20:58:10 +0300

    Integrate changes from custom pybind fork
    The fork was no longer being mantained, which led to it being severly outdated compared to the main pybind11 repository. Moving the changes from the fork into oead allows for compatibility with newer versions without breaking anything. Some other changes, such as overloading OeadGetSpanCasterName, were also made to allow for compatibility with newer versions.
    
    Merge branch 'master' of https://github.com/Nitr4m12/oead/commit/9e8ee15ed296af199fc7a40fe4ade7d0d67c62af
    
    also add test

--- test/aamp/test_parameter_map.py
+++ test/aamp/test_parameter_map.py
@@ -11,9 +11,11 @@ def test_parameter_map_mapping_operations():
     params = oead.aamp.ParameterMap()
     first = oead.aamp.Name("First")
     second = oead.aamp.Name("Second")
+    third = oead.aamp.Name("Third")
     missing = oead.aamp.Name("Missing")
     first_value = oead.aamp.Parameter(1)
     second_value = oead.aamp.Parameter(2)
+    third_value = oead.aamp.Parameter(3)
 
     params[first] = first_value
     assert len(params) == 1
@@ -33,8 +35,20 @@ def test_parameter_map_mapping_operations():
     assert list(params.values()) == [first_value, second_value]
     assert list(params.items()) == [(first, first_value), (second, second_value)]
 
+    keys = params.keys()
+    values = params.values()
+    items = params.items()
+    params[third] = third_value
+    assert list(keys) == [first, second, third]
+    assert list(values) == [first_value, second_value, third_value]
+    assert list(items) == [
+        (first, first_value),
+        (second, second_value),
+        (third, third_value),
+    ]
+
     del params[first]
-    assert len(params) == 1
+    assert len(params) == 2
     assert first not in params
     with pytest.raises(KeyError):
         del params[first]
